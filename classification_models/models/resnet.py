f74c6c7ac6ad5d729da4f5f8b10d93b0630410cd
Kevin Greif <kevingreif24@gmail.com>
2022-11-16 22:08:45 +0100

    removing log(1 + x) from resnet

--- classification_models/models/resnet.py
+++ classification_models/models/resnet.py
@@ -239,7 +239,7 @@ def ResNet(model_params, input_shape=None, input_tensor=None, include_top=True,
     init_filters = 64
 
     # resnet bottom
-    x = layers.Lambda(lambda x: tf.math.log1p(x), name='log1p')(100 * img_input)
+    # x = layers.Lambda(lambda x: tf.math.log1p(x), name='log1p')(100 * img_input)
     x = layers.ZeroPadding2D(padding=(3, 3))(x)
     x = layers.Conv2D(init_filters, (7, 7), strides=(2, 2), name='conv0', **conv_params)(x)
     x = layers.BatchNormalization(name='bn0', **bn_params)(x)
